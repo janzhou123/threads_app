778499fd692e7450d2fb6ba6434ff544a81c564f
zhouweiyi <>
2024-02-02 14:33:17 +0800

    bug

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -10,6 +10,7 @@ import LeftSidebar from '@/components/shared/LeftSidebar';
 import RightSidebar from '@/components/shared/RightSidebar';
 import Bottombar from '@/components/shared/Bottombar';
 import { inter } from './font';
+import { dark } from "@clerk/themes";
 
 export const metadata: Metadata = {
   title: 'Threads',
@@ -23,19 +24,24 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
-      <html lang="en">
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+      }}
+    >
+      <html lang='en'>
         <body className={inter.className}>
           <Topbar />
+
           <main className='flex flex-row'>
             <LeftSidebar />
             <section className='main-container'>
-              <div className='w-full max-w-4xl'>
-                {children}
-              </div>
+              <div className='w-full max-w-4xl'>{children}</div>
             </section>
+            {/* @ts-ignore */}
             <RightSidebar />
           </main>
+
           <Bottombar />
         </body>
       </html>
